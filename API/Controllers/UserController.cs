f833abd7045be0d286bed46555c0e3fed2aa9a54
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2024-09-20 14:43:40 -0300

    end of api

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+
+        public UserController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _contexto.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _contexto.Users.FindAsync(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutUser(int id, AppUser user)
+        {
+            _contexto.Entry(user).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> PostUser(AppUser user)
+        {
+            _contexto.Users.Add(user);
+            await _contexto.SaveChangesAsync();
+            return user;
+        }
+    }
+}
